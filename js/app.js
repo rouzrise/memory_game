e85feb7b9371d84addb3220a85afd1f2c76f9c91
Rouzrise <rouzrise@gmail.com>
2018-03-28 00:15:27 +0300

    Create Nodelist that holds all i-elems

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
-
+const cards = document.querySelectorAll('.deck i');
 
 /*
  * Display the cards on the page
@@ -11,19 +11,6 @@
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
 
 
 /*
