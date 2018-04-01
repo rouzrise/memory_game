5b9c55371817486db41e0506dc3b4f0a4b283227
Rouzrise <rouzrise@gmail.com>
2018-04-01 14:55:58 +0300

    Add changes on Udacity review_part 1
    Changes added:
    -array doubled instead of hardcoding two of each card in one array
    (using concat() method);
    -connected variables and constants are kept inside objects;
    -several classes added at once (more tidy code)

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
-let IconsArray = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o', 'fa-anchor', 'fa-anchor', 'fa-bolt', 'fa-bolt', 'fa-cube', 'fa-cube', 'fa-leaf', 'fa-leaf', 'fa-bicycle', 'fa-bicycle', 'fa-bomb', 'fa-bomb'];
-let totalMoves = 0;
-const moves = document.querySelector('.moves');
-let allMatch = document.getElementsByClassName('match');
-let minutes = 0;
-let seconds = 0;
-let openCards = [];
-const timer = document.querySelector('.timer');
-const modal = document.querySelector('.modal');
-const restartButton = document.querySelector('.restart-button');
+const gameConst = { 
+    initialArray: ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'],
+    moves: document.querySelector('.moves'),
+    timer: document.querySelector('.timer'),
+    modal: document.querySelector('.modal'),
+    restartButton: document.querySelector('.restart-button')
+}
+
+let gameLet = {
+    iconsArray: gameConst.initialArray.concat(gameConst.initialArray),
+    allMatch: document.getElementsByClassName('match'),
+    totalMoves: 0,
+    minutes: 0,
+    seconds: 0,
+    openCards: [],
+}
+
 let startTime;
 
 //shuffle cards
@@ -25,7 +32,7 @@ function shuffle(array) {
     return array;
 }
 
-shuffle(IconsArray);
+shuffle(gameLet.iconsArray);
 
 //create grid with elements
 function createNewGrid() {
@@ -39,8 +46,7 @@ function createNewGrid() {
         const elem = document.createElement('li');
         elem.classList.add('card');
         const iElem = document.createElement('i');
-        iElem.classList.add('fa');
-        iElem.classList.add(IconsArray[i]);
+        iElem.classList.add('fa', iconsArray[i]);
         elem.appendChild(iElem);
         testDeck.appendChild(elem);
     }
@@ -58,12 +64,12 @@ function respondToTheClick(e) {
     let card = e.target;
 
     // if none cards were open or one card was open before
-    if (card.nodeName === 'LI' && card.classList.contains('open') === false && openCards.length < 2) {
+    if (card.nodeName === 'LI' && card.classList.contains('open') === false && gameLet.openCards.length < 2) {
         card.classList.add('show', 'open');
-        openCards.push(card);
+        gameLet.openCards.push(card);
         
         //start timer
-        if (totalMoves === 1) {
+        if (gameLet.totalMoves === 1) {
             stopWatch (); 
         }
 
@@ -71,56 +77,56 @@ function respondToTheClick(e) {
         ratingDisplay ();
 
         //if cards match
-        if (openCards.length > 1 && openCards[0].lastChild.className === openCards[1].lastChild.className) {
+        if (gameLet.openCards.length > 1 && gameLet.openCards[0].lastChild.className === gameLet.openCards[1].lastChild.className) {
             match();
-            totalMoves += 1;
-            moves.innerText = `${totalMoves}`;
+            gameLet.totalMoves += 1;
+            gameConst.moves.innerText = `${gameLet.totalMoves}`;
             
         }
         //if cards don't match
-        else if (openCards.length > 1 && openCards[0].lastChild.className != openCards[1].lastChild.className) {
+        else if (gameLet.openCards.length > 1 && gameLet.openCards[0].lastChild.className != gameLet.openCards[1].lastChild.className) {
             setTimeout(makeRed, 200);
             setTimeout(removeRed, 700);  
-            totalMoves += 1;
-            moves.innerText = `${totalMoves}`;
+            gameLet.totalMoves += 1;
+            gameConst.moves.innerText = `${gameLet.totalMoves}`;
         }
     }
  
     function match () {
-        openCards[0].classList.add('match');
-        openCards[1].classList.add('match');
-        openCards[0].classList.remove('open', 'show');
-        openCards[1].classList.remove('open', 'show');
-        openCards.length = 0;
+        gameLet.openCards[0].classList.add('match');
+        gameLet.openCards[1].classList.add('match');
+        gameLet.openCards[0].classList.remove('open', 'show');
+        gameLet.openCards[1].classList.remove('open', 'show');
+        gameLet.openCards.length = 0;
     }
 
     function makeRed () {
-        openCards[0].classList.add('makeRed');
-        openCards[1].classList.add('makeRed');
-        openCards[0].classList.remove('open', 'show');
-        openCards[1].classList.remove('open', 'show');
+        gameLet.openCards[0].classList.add('makeRed');
+        gameLet.openCards[1].classList.add('makeRed');
+        gameLet.openCards[0].classList.remove('open', 'show');
+        gameLet.openCards[1].classList.remove('open', 'show');
     }
 
     function removeRed () {
-        openCards[0].classList.remove('makeRed');
-        openCards[1].classList.remove('makeRed');
-        openCards.length = 0;
+        gameLet.openCards[0].classList.remove('makeRed');
+        gameLet.openCards[1].classList.remove('makeRed');
+        gameLet.openCards.length = 0;
     }
 
     //display rating - makes 2 golden stars left if total clicks are equal or more then 30 /
     //and 1 golden star left if total clicks are equal or more then 50 (makes other stars white)
     function ratingDisplay () { 
         const starsContainer = document.querySelector('.stars');
-        if (totalMoves === 30) {
+        if (gameLet.totalMoves === 30) {
             starsContainer.children[2].lastChild.className = 'whiteColor fa fa-star white';
         }
-        else if (totalMoves === 50) {
+        else if (gameLet.totalMoves === 50) {
             starsContainer.children[1].lastChild.className = 'whiteColor fa fa-star white';
         }
     }
 
     //after all cards are matched
-    if (allMatch.length === 16) {
+    if (gameLet.allMatch.length === 16) {
         //evokes modal window
         modalOpen();
     }
@@ -132,7 +138,7 @@ function respondToTheClick(e) {
         const rating = document.querySelector('.finalRating');
         const timeSpent = document.querySelector('.timeSpent');
 
-        modal.classList.toggle('closed');
+        gameConst.modal.classList.toggle('closed');
 
         //checks golden stars left to show their quantity on screen
         let starsQuantity = document.querySelectorAll('.whiteColor');
@@ -141,22 +147,22 @@ function respondToTheClick(e) {
             rating.innerHTML += '<li><i class="fa fa-star"></i></li>';
         }
 
-        timeSpent.innerText = `${minutes} : ${seconds-1}`;
+        timeSpent.innerText = `${gameLet.minutes} : ${gameLet.seconds-1}`;
 
         //functionality after 'Once more?' button clicked
-        restartButton.addEventListener('click', function(e) {
+        gameConst.restartButton.addEventListener('click', function(e) {
             e.preventDefault();
-            modal.classList.toggle('closed');
+            gameConst.modal.classList.toggle('closed');
             document.querySelector('.deck').remove();
-            shuffle(IconsArray);
+            shuffle(gameLet.iconsArray);
             createNewGrid();
-            totalMoves = 0;
+            gameLet.totalMoves = 0;
             clearInterval(startTime);
-            minutes = 0;
-            seconds = 0;
-            moves.innerText = '0';
+            gameLet.minutes = 0;
+            gameLet.seconds = 0;
+            gameConst.moves.innerText = '0';
             document.querySelector('.stars').innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
-            timer.innerText = '0 : 0';
+            gameConst.timer.innerText = '0 : 0';
         });
     
     }
@@ -165,11 +171,11 @@ function respondToTheClick(e) {
 //stopwatch functionality
 function stopWatch () {
     startTime = setInterval(function start() {
-        timer.innerText = minutes + ' : ' + seconds;  
-        seconds++; 
-        if (seconds === 60) {
-            minutes++;
-            seconds = 0;
+        gameConst.timer.innerText = gameLet.minutes + ' : ' + gameLet.seconds;  
+        gameLet.seconds++; 
+        if (gameLet.seconds === 60) {
+            gameLet.minutes++;
+            gameLet.seconds = 0;
         }
     }, 1000);
 }
@@ -180,15 +186,15 @@ const restart = document.querySelector('.restart');
 restart.addEventListener('click', function(e) {
     e.preventDefault();
     document.querySelector('.deck').remove();
-    shuffle(IconsArray);
+    shuffle(gameLet.iconsArray);
     createNewGrid();
-    totalMoves = 0;
+    gameLet.totalMoves = 0;
     clearInterval(startTime);
-    moves.innerText = '0';
+    gameConst.moves.innerText = '0';
     document.querySelector('.stars').innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
-    timer.innerText = '0 : 0';
-    minutes = 0;
-    seconds = 0;
+    gameConst.timer.innerText = '0 : 0';
+    gameLet.minutes = 0;
+    gameLet.seconds = 0;
 });
  
 
