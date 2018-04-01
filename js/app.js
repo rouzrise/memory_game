f1d6ef7b86b53e256f29cf0883cb4ffed0f19ea2
Rouzrise <rouzrise@gmail.com>
2018-04-01 04:14:15 +0300

    Add final functionality
    Final functionality to submit project for review added. Code is tidy and
    structured.
    'Open card'-Bugs fixed (In previous versions when you firstly open the
    card it stayed open before you could find its match. Now when you open
    two cards and they don't match - they both become red and then close.
    Also it was possible to open more then two unmatched cards at once in
    previous versions - fixed now.)
    Restart-Bug fixed (earlier after pressing restart button the grid was
    created but eventListener was not added to it - and you could not open
    cards. The same for 'restart' button in modal window. Now - fixed).
    Grid wz cards deleted from initial html-file - now it is only created
    dinamically wz JS.

--- js/app.js
+++ js/app.js
@@ -1,193 +1,194 @@
-let cards = document.querySelectorAll('.deck i');
-let newArray = [];
-let openCards = [];
+let IconsArray = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o', 'fa-anchor', 'fa-anchor', 'fa-bolt', 'fa-bolt', 'fa-cube', 'fa-cube', 'fa-leaf', 'fa-leaf', 'fa-bicycle', 'fa-bicycle', 'fa-bomb', 'fa-bomb'];
+let totalMoves = 0;
+const moves = document.querySelector('.moves');
+let allMatch = document.getElementsByClassName('match');
 let minutes = 0;
 let seconds = 0;
+let openCards = [];
+const timer = document.querySelector('.timer');
+const modal = document.querySelector('.modal');
+const restartButton = document.querySelector('.restart-button');
+let startTime;
 
+//shuffle cards
 function shuffle(array) {
-    let currentIndex = array.length;
-    let temporaryValue;
-    let randomIndex;
-
-    for (let i = 0; i <=15; i++) {
-        newArray.push(array[i]);
-    }
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
-        temporaryValue = newArray[currentIndex];
-        newArray[currentIndex] = newArray[randomIndex];
-        newArray[randomIndex] = temporaryValue;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
 
-    return newArray;
+    return array;
 }
 
-shuffle(cards); 
+shuffle(IconsArray);
 
+//create grid with elements
 function createNewGrid() {
-    let testDeck = document.createElement('div');
-    let container = document.querySelector('.container');
+    const testDeck = document.createElement('ul');
+    const container = document.querySelector('.container');
+    const deck = document.querySelector('.deck');
 
-    document.querySelector('.deck').remove();
     testDeck.classList.add('deck');
-    
+
     for (let i = 0; i < 16; i++) {
         const elem = document.createElement('li');
         elem.classList.add('card');
-        elem.appendChild(newArray[i]);
+        const iElem = document.createElement('i');
+        iElem.classList.add('fa');
+        iElem.classList.add(IconsArray[i]);
+        elem.appendChild(iElem);
         testDeck.appendChild(elem);
     }
-
+    
     container.appendChild(testDeck);
+    testDeck.addEventListener('click', respondToTheClick);
 }
 
 createNewGrid();
 
-const deck = document.querySelector('.deck');
-const matches = document.getElementsByClassName('open');
-let totalOpen = document.getElementsByClassName('match');
-let totalMoves = 0;
-const moves = document.querySelector('.moves');
+//creates functionality after click on the card
+function respondToTheClick(e) {
+    e.preventDefault();
 
-deck.addEventListener('click', function (e) {
-   e.preventDefault();
     let card = e.target;
 
-    if (card.nodeName === 'LI' && card.classList.contains('open') === false) {
-    card.classList.add('show', 'open');
-    totalMoves += 1;
-    moves.innerText = `${totalMoves}`;
-    openCards.push(card);
-    }
-
-    if (totalMoves === 1) {
-        
-        stopWatch (); 
-    }
+    // if none cards were open or one card was open before
+    if (card.nodeName === 'LI' && card.classList.contains('open') === false && openCards.length < 2) {
+        totalMoves += 1;
+        card.classList.add('show', 'open');
+        moves.innerText = `${totalMoves}`;
+        openCards.push(card);
     
-    function openingCards() {
-        if (openCards.length > 1 && openCards[0].lastChild.className === openCards[1].lastChild.className) {
-    
-            for(let i=0; i < matches.length; i++) {
-                if (matches[i] === openCards[0] || matches[i] === openCards[1]) {
-                matches[i].classList.add('match');
-                }
-            }
-    
-            openCards.length=0;
-
-        }
-    
-        else if (openCards.length > 1 && openCards[0].lastChild.className !== openCards[1].lastChild.className) {
-   
-        function removing () {
-        card.classList.remove('show', 'open');
+        //start timer
+        if (totalMoves === 1) {
+            stopWatch (); 
         }
 
-        setTimeout(removing, 500);
-        openCards.pop();
-        }
     }
 
-    openingCards();
-
-    ///StopWatch functionality
-    
-
-    function stopWatch () {
-        startTime = setInterval(function start() {
-            time.innerText = minutes + ' : ' + seconds;
-            seconds++;
-            if (seconds === 60) {
-                minutes++;
-                seconds = 0;
-            }
-        }, 1000);
-    }
-
-    function modalWindow() {
-    const rating = document.querySelector('.finalRating');
-    const timeSpent = document.querySelector('.timeSpent');
-    
-
-    if (totalOpen.length === 16) {
-        clearInterval(startTime);
-
-        const modal = document.querySelector('#modal');
-        const restartButton = document.querySelector('#restart-button');
-
-        modal.classList.toggle('closed');
-        rating.innerHTML = document.querySelector('.stars').innerHTML;
-        timeSpent.innerText = `${minutes} : ${seconds-1}`;
-
-        restartButton.addEventListener('click', function(e) {
-            e.preventDefault();
-            newArray = [];
-            shuffle(cards);
-            createNewGrid();
-            clearInterval(startTime);
-            totalMoves = 0;
-            moves.innerText = '0';
-            modal.classList.toggle('closed');
-            document.querySelector('.stars').innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
-            time.innerText = '0 : 0';
-            // restart();
-            });
-        }
-    }
-
-    modalWindow();   
-
-///Rating 
-
-    function ratingDisplay () { 
-        const starsContainer = document.querySelector('.stars');
-        const stars = document.querySelector('.stars li');
-
-        if (totalMoves === 30) {
-            starsContainer.removeChild(stars);
-        }
-
-        else if (totalMoves === 50) {
-            starsContainer.removeChild(stars);
-        }
-    }
-
+    //evoke rating count 
     ratingDisplay ();
 
-});
+    //if cards match
+    if (openCards.length > 1 && openCards[0].lastChild.className === openCards[1].lastChild.className) {
+        match();
+    }
+    //if cards don't match
+    else if (openCards.length > 1 && openCards[0].lastChild.className != openCards[1].lastChild.className) {
+        setTimeout(makeRed, 200);
+        setTimeout(removeRed, 700);  
+    }
+ 
+    function match () {
+        openCards[0].classList.add('match');
+        openCards[1].classList.add('match');
+        openCards[0].classList.remove('open', 'show');
+        openCards[1].classList.remove('open', 'show');
+        openCards.length = 0;
+    }
+
+    function makeRed () {
+        openCards[0].classList.add('makeRed');
+        openCards[1].classList.add('makeRed');
+        openCards[0].classList.remove('open', 'show');
+        openCards[1].classList.remove('open', 'show');
+    }
+
+    function removeRed () {
+        openCards[0].classList.remove('makeRed');
+        openCards[1].classList.remove('makeRed');
+        openCards.length = 0;
+    }
+
+    //display rating - makes 2 golden stars left if total clicks are equal or more then 30 /
+    //and 1 golden star left if total clicks are equal or more then 50 (makes other stars white)
+    function ratingDisplay () { 
+        const starsContainer = document.querySelector('.stars');
+        if (totalMoves === 30) {
+            starsContainer.children[2].lastChild.className = 'whiteColor fa fa-star white';
+        }
+        else if (totalMoves === 50) {
+            starsContainer.children[1].lastChild.className = 'whiteColor fa fa-star white';
+        }
+    }
+
+    //after all cards are matched
+    if (allMatch.length === 16) {
+        //evokes modal window
+        modalOpen();
+    }
+
+    //describes modal window functionality
+    function modalOpen () {
+        clearInterval(startTime);
+
+        const rating = document.querySelector('.finalRating');
+        const timeSpent = document.querySelector('.timeSpent');
+
+        modal.classList.toggle('closed');
+
+        //checks golden stars left to show their quantity on screen
+        let starsQuantity = document.querySelectorAll('.whiteColor');
+        rating.innerHTML = '';
+        for (let i = 0; i < (3 - starsQuantity.length); i++) {
+            rating.innerHTML += '<li><i class="fa fa-star"></i></li>';
+        }
+
+        timeSpent.innerText = `${minutes} : ${seconds-1}`;
+
+        //functionality after 'Once more?' button clicked
+        restartButton.addEventListener('click', function(e) {
+            e.preventDefault();
+            modal.classList.toggle('closed');
+            document.querySelector('.deck').remove();
+            shuffle(IconsArray);
+            createNewGrid();
+            totalMoves = 0;
+            clearInterval(startTime);
+            minutes = 0;
+            seconds = 0;
+            moves.innerText = '0';
+            document.querySelector('.stars').innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
+            timer.innerText = '0 : 0';
+        });
+    
+    }
+}
+
+//stopwatch functionality
+function stopWatch () {
+    startTime = setInterval(function start() {
+        timer.innerText = minutes + ' : ' + seconds;  
+        seconds++; 
+        if (seconds === 60) {
+            minutes++;
+            seconds = 0;
+        }
+    }, 1000);
+}
 
 const restart = document.querySelector('.restart');
-restart.addEventListener('click', function (e) {
+
+//functionality after 'Restart' button clicked
+restart.addEventListener('click', function(e) {
     e.preventDefault();
-    cards = document.querySelectorAll('.deck i');
-    newArray = [];
-    openCards = [];
-    shuffle(cards);
-    function createNewGrid() {
-        let testDeck = document.createElement('div');
-        let container = document.querySelector('.container');
-    
-        document.querySelector('.deck').remove();
-        testDeck.classList.add('deck');
-        
-        for (let i = 0; i < 16; i++) {
-            const elem = document.createElement('li');
-            elem.classList.add('card');
-            elem.appendChild(newArray[i]);
-            testDeck.appendChild(elem);
-        }
-    
-        container.appendChild(testDeck);
-    };
+    document.querySelector('.deck').remove();
+    shuffle(IconsArray);
     createNewGrid();
     totalMoves = 0;
     clearInterval(startTime);
     moves.innerText = '0';
     document.querySelector('.stars').innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
-    time.innerText = '0 : 0';
-    
+    timer.innerText = '0 : 0';
+    minutes = 0;
+    seconds = 0;
 });
+ 
 
+ 
+     
